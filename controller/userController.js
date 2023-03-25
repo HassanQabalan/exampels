cedd11dff4a5dfb728a9f19a2b03ec2f15968a4a
Hassan Qabalan <54742600+HassanQabalan@users.noreply.github.com>
2023-03-26 01:18:35 +0200

    first co

--- controller/userController.js
+++ controller/userController.js
@@ -109,18 +109,18 @@ var apis={
 
     register_exampels: async(req,res)=>{
         try{
-            const user = await User.findOne({phone:req.body.phone});
-            const userbyphone = await User.findOne({email:req.body.email});
-            if(user){
-                return res.json({success:false,msg:'This email is already exists'})
-             }
-            else if(userbyphone){
-               return res.json({success:false,msg:'This phone is already exists'})
-            }else{
+            // const user = await User.findOne({phone:req.body.phone});
+            // const userbyphone = await User.findOne({email:req.body.email});
+            // if(user){
+            //     return res.json({success:false,msg:'This email is already exists'})
+            //  }
+            // else if(userbyphone){
+            //    return res.json({success:false,msg:'This phone is already exists'})
+            // }else{
              const newUser =new User({
                 name:"hasan",
                 email:"aaaa@gmail.com",
-                phone:"0797981863",
+                phone:"0797981868",
                 profileImage:"",
                 password:"nnkl",
                 birthdate:"1222",
@@ -130,7 +130,7 @@ var apis={
             const savedUser= await newUser.save();
             const token = jwt.sign({ userId: savedUser._id }, process.env.JWT_SECRET);
             res.json({success:true, user:savedUser, token})
-            }
+            // }
         }catch(error){
             res.json({success:false,msg:error.message})
         }
